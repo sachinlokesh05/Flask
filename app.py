84cfd653888cac0651f4798b4dc6b17e1bd6e994
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-02-10 11:09:14 +0530

    [sachin]added.template and static folders

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
+posts = [{
+    'title': 'flask',
+    'content': 'my first flask app',
+    'author': 'sachin'
+}, {
+
+    'title': 'django',
+    'content': 'hello world',
+    'author': 'sanju'
+}
+]
+
+
 @app.route("/")
+@app.route("/home")
 def home():
-    return "Hello, Flask!"
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', posts=posts)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
